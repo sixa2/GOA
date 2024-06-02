ab2170c0cce4330b222bb8a1e1cfe395ad146a05
sixa2 <dotsixa@gmail.com>
2024-06-02 15:02:14 +0400

    GOA

---
+++ Level 48/Homework/lesson48.py
@@ -0,0 +1,27 @@
+#################################
+
+# def check_exam(arr1,arr2):
+#     score = 0
+#     length = len(arr1)
+    
+#     for i in range (length):
+#         correct = arr1[i]
+#         student = arr2[i]
+        
+#         if student == correct:
+#             score += 4 
+#         elif student == "":
+#             score += 0
+#         else:
+#             score -= 1
+#     return max(score,0)
+
+
+#################################
+
+
+# def get_even_numbers(arr):
+#     return [num for num in arr if num % 2 == 0]
+
+
+#################################
